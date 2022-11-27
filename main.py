4847c9751ef869a963ed352a8dbea0d5339fc0d2
Sungyu Kwon <kwonsun7@msu.edu>
2022-11-27 02:05:02 -0500

    Trying to get the gradescope to parse my data

--- main.py
+++ main.py
@@ -1,73 +1,38 @@
-# importing copy module
-import copy
-
 number_of_agents, number_of_distinct_skills = input().split()
 number_of_agents = int(number_of_agents)
 number_of_distinct_skills = int(number_of_distinct_skills)
 
-required_skills = input()
-required_skills_list = [str(i) for i in required_skills.split(' ')]
-
-#trying a list
 agents = []
 
-for i in range(0, number_of_agents):
-    skills_num = values = input()
-    skills = input()
-    skills_list = [i for i in skills.split(' ')]
-    agents.append(skills_list)
+for i in range(0,number_of_agents):
+    skills_num = input()
+    distinct_skills = input()
+    #print(distinct_skills)
+    skills = [int(c) for c in distinct_skills.split(' ')]
+    #print(skills)
+    #skills = distinct_skills
 
-#now doing brute force
-minimum_people = number_of_agents   #watch out this could cause errors
+    agents.append([skills_num,skills, i])
 
-for i in range(0, len(agents)):
+#this is causing the error
+agents.sort(key=lambda x: x[0]) #sorted in reverse order
 
-    #new agent number tracker and list
-    current_agent_number = 1
-    required_skills_list2 = copy.deepcopy(required_skills_list)
+skills_set = set()
+i =len(agents) -1
+agents_chosen = []
+while len(skills_set) < number_of_distinct_skills:
+    for j in range(0, len(agents[i][1])):
+        debug = agents[i][1][j]
+        skills_set.add(agents[i][1][j])
+    agents_chosen.append(agents[i][2])
+    i = i-1
 
-    #check for current agent
-    for x in range(0, len(agents[i])):
-        if agents[i][x] in required_skills_list2:
-            required_skills_list2.remove(agents[i][x])
-
-    if len(required_skills_list2) == 0:
-        minimum_people = 1
-        break
-
-    for j in range(0, len(agents)):
-        current_agent_number = current_agent_number+1
-        if(current_agent_number >= minimum_people):
-            break
-        elif i != j:
-            for x in range(0, len(agents[j])):
-                if agents[j][x] in required_skills_list2:
-                    required_skills_list2.remove(agents[j][x])
-
-            if len(required_skills_list2) == 0:
-                minimum_people = current_agent_number
-                break
-
-print(minimum_people)
+print(len(agents_chosen))
+print(agents_chosen)
 
 
 
 
-"""
-while(len(required_skills_list2) > 0):
-    current_agent_number = 0;
-    for i in range(0, len(agents)):
-        current_agent_number = current_agent_number + 1
-        for x in range(0, len(agents[i])):
-            if agents[x] in required_skills_list2:
-                required_skills_list2.remove(agents[x])
-
-        if len(required_skills_list2) == 0:
-
-
-        for j in range(0, len(agents)):
-            if(i != j):
-            """
 
 
 
